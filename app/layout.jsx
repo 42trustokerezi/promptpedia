d26ea0c4ecde77402da867f0a39d422e483a7711
42trustokerezi <trustokerezi42@gmail.com>
2025-01-06 13:10:41 +0100

    suspense wrapping

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,25 +1,28 @@
 import "@styles/globals.css";
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
+import { Suspense } from "react";
 
 export const metadata = {
   title: "Promptpedia",
   description: "Dsicover & Share AI Prompts",
 };
 
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-      <Provider>
-        <div className="main">
-          <div className="gradient" />
-        </div>
+        <Provider>
+          <Suspense fallback={null}>
+            <div className="main">
+              <div className="gradient" />
+            </div>
 
-        <main className="app">
-        <Nav/>
-        {children}
-        </main>
+            <main className="app">
+              <Nav />
+              {children}
+            </main>
+          </Suspense>
         </Provider>
       </body>
     </html>
